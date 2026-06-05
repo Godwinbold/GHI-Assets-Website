5b0e90404cc2b20aca0fdd99c2ddaccf1d00a3c1
Samuel3310 <awoluowolabisamuel@gmail.com>
2026-06-05 11:14:30 +0100

    feat: add navigation structure and routing for main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Routes, Route, Link, NavLink } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import ServiceDetail from './pages/ServiceDetail'
+import Insights from './pages/Insights'
+import InsightDetail from './pages/InsightDetail'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+import Footer from './components/home/Footer'
+import Partners from './pages/Partners'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div>
+      {/* Navigation */}
+      <nav className="bg-white/80 fixed w-full top-0 left-0 z-50">
+        <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
+          <Link to="/" className="text-2xl font-bold text-brand">
+           <img src='/images/logo.png' className='w-38.5 h-13.5'/>
+          </Link>
+          <div className="flex gap-8">
+            <NavLink to="/" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>Home</NavLink>
+            <NavLink to="/services" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>Services</NavLink>
+            <NavLink to="/partners" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>Our Partners</NavLink>
+            <NavLink to="/insights" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>Insights</NavLink>
+            <NavLink to="/about" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>About Us</NavLink>
+            <NavLink to="/contacts" className={({ isActive }) => `nav-link transition ${isActive ? 'text-brand active' : 'hover:text-brand/85'}`}>Contacts</NavLink>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      </nav>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Partners />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:slug" element={<ServiceDetail />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/insights/:slug" element={<InsightDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
+
+
 
 export default App
